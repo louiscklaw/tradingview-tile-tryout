f9c5405f74d285ba4dea1f506e8d2bf2ffbd8a36
louiscklaw <louiscklaw@gmail.com>
2019-09-12 18:27:49 +0800

    working update,

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ function css() {
 }
 
 function js() {
-  return src('src/pug/*.js', { sourcemaps: true })
+  return src('src/pug/app.js', { sourcemaps: true })
     // .pipe(concat('app.min.js'))
     .pipe(dest('./docs/js', { sourcemaps: true }))
 }
